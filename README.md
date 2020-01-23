b5046725298ba1643a3124da41760d402f0c932f
Tigr <t.sarkisyan@turkmen-tranzit.com>
2020-01-23 10:57:13 +0500

    Whatsapp api linki generation feature. READEME add step

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Whatsapp chat widget (HTML, CSS, JS)
 
 1. Include whatsapp.css in the header.
 2. Include whatsapp.js	just before </body> tag.
-3. Include widget container just before whatsapp.js that you included in the 2nd step. 
+3. Include widget container just before whatsapp.js that you included in the 2nd step.
+4. Set variable phone in the whatsapp.js. Whatsapp phone number must be without "+".
 
 Enjoy!
--- whatsapp.js
+++ whatsapp.js
@@ -1,6 +1,12 @@
 let btn = document.getElementById('whatsapp-btn');
 let chat = document.getElementById('chat-container');
-let placeTime = document.getElementById("time");
+let placeTime = document.getElementById('time');
+let phoneLink = document.getElementById('whatsapp-link');
+let phone = '99362920755'
+
+window.onload = function(){
+    generateLink(phone);
+}
 
 function toggle_chat(){
     chat.classList.toggle('active');
@@ -14,4 +20,9 @@ function getFormatedTime() {
     let curMinute =	date.getMinutes();
     let totalMinute = curMinute < 10 ? '0' + curMinute : curMinute;
     return totalHours + ':' + totalMinute;
+}
+
+function generateLink(p) {
+    let link = 'https://api.whatsapp.com/send?phone=' + phone;
+    phoneLink.setAttribute('href', link)
 }
